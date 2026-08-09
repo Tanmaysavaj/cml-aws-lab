60b6d19900a53f9968266e3a16970fde36c687f7
Tanmay Savaj <tanmaysavaj5862@gmail.com>
2026-08-08 21:24:56 -0400

    Add Terraform variable and version configuration files for AWS infrastructure

---
+++ infrastructure/variables.tf
@@ -0,0 +1,51 @@
+variable "aws_region" {
+  description = "AWS region where the CML lab will be deployed"
+  type        = string
+  default     = "ca-central-1"
+}
+
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "cml-aws-lab"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "lab"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the CML VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type used for CML"
+  type        = string
+  default     = "c8i.8xlarge"
+}
+
+variable "root_volume_size" {
+  description = "Root EBS volume size in GB"
+  type        = number
+  default     = 200
+}
+
+variable "allowed_management_cidr" {
+  description = "Public IPv4 CIDR allowed to access CML management and SSH"
+  type        = string
+
+  validation {
+    condition     = can(cidrhost(var.allowed_management_cidr, 0))
+    error_message = "allowed_management_cidr must be a valid IPv4 CIDR, for example 203.0.113.10/32."
+  }
+}
